9ba7700e0350db77bc3a809f18e4f5337e6610a4
Marek Grzyb <b1narykill3r@gmail.com>
2018-07-02 19:10:56 +0200

    Added UpdateStats handler for both client and server

--- Framework/Client/Fesl/rank.py
+++ Framework/Client/Fesl/rank.py
@@ -34,8 +34,34 @@ def HandleGetStats(self, data):
     Packet(toSend).send(self, "rank", 0xC0000000, self.CONNOBJ.plasmaPacketID)
 
 
+def HandleUpdateStats(self, data):
+    toSend = Packet().create()
+    toSend.set("PacketData", "TXN", "UpdateStats")
+
+    users = int(data.get("PacketData", "u.[]"))
+
+    for user in range(users):
+        owner = int(data.get("PacketData", "u." + str(user) + ".o"))
+
+        statsToUpdate = int(data.get("PacketData", "u." + str(user) + ".s.[]"))
+
+        for stat in range(statsToUpdate):
+            key = str(data.get("PacketData", "u." + str(user) + ".s." + str(stat) + ".k"))
+            value = str(data.get("PacketData", "u." + str(user) + ".s." + str(stat) + ".v"))
+            text = str(data.get("PacketData", "u." + str(user) + ".s." + str(stat) + ".t"))
+
+            if key == 'c_ltm' or key == 'c_slm' or key == 'c_tut':
+                db.updateStat(owner, "account", key, value, text)
+            else:
+                db.updateStat(owner, "persona", key, value, text)
+
+    Packet(toSend).send(self, "rank", 0xC0000000, self.CONNOBJ.plasmaPacketID)
+
+
 def ReceivePacket(self, data, txn):
     if txn == 'GetStats':
         HandleGetStats(self, data)
+    elif txn == 'UpdateStats':
+        HandleUpdateStats(self, data)
     else:
         self.logger_err.new_message("[" + self.ip + ":" + str(self.port) + ']<-- Got unknown rank message (' + txn + ")", 2)
